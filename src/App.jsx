a6e605529456c3bf05b21c8545d438f42bc89357
Muhammad hassan <hassanshahid5218@gmail.com>
2026-03-29 11:39:30 +0500

    update app.jsx again

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@
 import React from 'react'
 import { BrowserRouter , Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './Pages/About'
+import About from './pages/About'
 import Signin from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
